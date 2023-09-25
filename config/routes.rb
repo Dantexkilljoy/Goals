dd776fe71a98e0381a0a2fa7c20c66a74e99d85e
Dustin Doyle <126806607+Dantexkilljoy@users.noreply.github.com>
2023-09-25 21:28:54 +0000

    re oraganized routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,16 @@
 Rails.application.routes.draw do
-  # Routes for the Goal resource:
+  root "goals#index"
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # CREATE
   post("/insert_goal", { :controller => "goals", :action => "create" })
-          
-  # READ
+ 
   get("/goals", { :controller => "goals", :action => "index" })
   
   get("/goals/:path_id", { :controller => "goals", :action => "show" })
   
-  # UPDATE
-  
   post("/modify_goal/:path_id", { :controller => "goals", :action => "update" })
   
-  # DELETE
   get("/delete_goal/:path_id", { :controller => "goals", :action => "destroy" })
-
-  #------------------------------
-
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root "goals#index"
+  
 end
