1f2d4b81a1812f8e874cd22e67b0c6a371be25db
Dustin Doyle <126806607+Dantexkilljoy@users.noreply.github.com>
2023-09-25 21:39:07 +0000

    edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,64 +1,42 @@
 Rails.application.routes.draw do
-  # Routes for the Celebration resource:
-
-  # CREATE
+  root "goals#index"
+  
+  devise_for :users
+  
   post("/insert_celebration", { :controller => "celebrations", :action => "create" })
           
-  # READ
   get("/celebrations", { :controller => "celebrations", :action => "index" })
   
   get("/celebrations/:path_id", { :controller => "celebrations", :action => "show" })
   
-  # UPDATE
-  
   post("/modify_celebration/:path_id", { :controller => "celebrations", :action => "update" })
   
-  # DELETE
   get("/delete_celebration/:path_id", { :controller => "celebrations", :action => "destroy" })
 
-  #------------------------------
 
-  # Routes for the Step resource:
-
-  # CREATE
+  
   post("/insert_step", { :controller => "steps", :action => "create" })
           
-  # READ
   get("/steps", { :controller => "steps", :action => "index" })
   
   get("/steps/:path_id", { :controller => "steps", :action => "show" })
   
-  # UPDATE
-  
   post("/modify_step/:path_id", { :controller => "steps", :action => "update" })
   
-  # DELETE
   get("/delete_step/:path_id", { :controller => "steps", :action => "destroy" })
 
-  #------------------------------
 
-  # Routes for the Action plan resource:
-
-  # CREATE
+  
   post("/insert_action_plan", { :controller => "action_plans", :action => "create" })
           
-  # READ
   get("/action_plans", { :controller => "action_plans", :action => "index" })
   
   get("/action_plans/:path_id", { :controller => "action_plans", :action => "show" })
   
-  # UPDATE
-  
   post("/modify_action_plan/:path_id", { :controller => "action_plans", :action => "update" })
   
-  # DELETE
   get("/delete_action_plan/:path_id", { :controller => "action_plans", :action => "destroy" })
 
-  #------------------------------
-
-  root "goals#index"
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   post("/insert_goal", { :controller => "goals", :action => "create" })
  
